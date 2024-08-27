6f17b734e7fadfd726182197815f658caa039b7a
Sebastien Matharel <smatharel@gmail.com>
2024-08-27 22:00:37 +0200

    update theme

--- static/js/checklist.js
+++ static/js/checklist.js
@@ -1,5 +1,24 @@
-var buttons = document.getElementsByClassName('uncheck-all');
+const buttons = document.querySelectorAll('.uncheck-all');
 
-buttons[0].onclick = function(){
-	document.querySelectorAll('input[type=checkbox]:checked').forEach(el => el.click());
-};
+console.log(buttons); //.parentNode;
+
+buttons.forEach((el) => {
+	el.onclick = function() {
+		console.log(el); //.parentNode;
+		const section = el.parentNode;
+		console.log(section);
+		const arrays = section.querySelectorAll('input[type=checkbox]:checked');
+		console.log(arrays);
+		arrays.forEach(function(button) {
+			button.click();
+		});
+		// const checked = el.parentNode.querySelectalAll('input[type=checkbox]:checked').forEach(e => e.click());
+	};
+});
+// get parent div
+// uncheck all
+
+
+// buttons[0].onclick = function(){
+//	document.querySelectorAll('input[type=checkbox]:checked').forEach(el => el.click());
+// };
